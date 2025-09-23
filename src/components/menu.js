3163748c0c77dac23c6aaed903cc984b00ff85d3
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-09-23 11:08:47 +0900

    Added functionality for showing menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,14 +1,33 @@
+// cuisine-type > title + food-container > food-item
+
 export function CreateCuisine(foodTitle){
   const content = document.getElementById("content")
-  let newCuisine = document.createElement("div")
+  const newCuisine = document.createElement("div")
   newCuisine.classList.add("content-div")
+  newCuisine.classList.add("menu-div")
   newCuisine.id = foodTitle.toLowerCase()
   content.appendChild(newCuisine)
-  let CuisineType = document.createElement("p")
+  const CuisineType = document.createElement("p")
   CuisineType.textContent = foodTitle
   newCuisine.appendChild(CuisineType)
+
+  const foodContainer = document.createElement("div")
+  foodContainer.classList.add("food-container")
+  foodContainer.id = foodTitle.toLowerCase() + "-container"
+  newCuisine.appendChild(foodContainer)
 }
 
-export function CreateFoodCard(newCuisine, foodTitle, Image){
-  //
+export function CreateFoodItem(foodContainer, foodTitle){
+  const foodItem = document.createElement("div")
+  foodItem.id = foodTitle.toLowerCase()
+  foodItem.classList.add("food-item")
+  foodContainer.appendChild(foodItem)
+  const foodName = document.createElement("p")
+  foodName.textContent = foodTitle
+  foodItem.appendChild(foodName)
+}
+
+export function CreateMenuContent(){
+  const foodItem = document.createElement("div")
+  foodItem.classList.add("food-item")
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.css"
 import { CreateHomeContent } from "./components/home"
-import {CreateCuisine, CreateFoodCard} from "./components/menu"
+import {CreateCuisine, CreateFoodItem, CreateMenuContent} from "./components/menu"
 
 const homeButton = document.getElementById("home-button")
 const menuButton = document.getElementById("menu-button")
@@ -22,7 +22,24 @@ homeButton.addEventListener("click", function(){
 
 menuButton.addEventListener("click", function(){
   DeleteDiv()
+
+  CreateCuisine("Italian")
+  const italianContainer = document.getElementById("italian-container")
+  CreateFoodItem(italianContainer, "Pizza")
+  CreateFoodItem(italianContainer, "Pasta")
+  CreateFoodItem(italianContainer, "Gnocchi")
+
+  CreateCuisine("Chinese")
+  const chineseContainer = document.getElementById("chinese-container")
+  CreateFoodItem(chineseContainer, "Dim Sum")
+  CreateFoodItem(chineseContainer, "Crispy Noodles")
+  CreateFoodItem(chineseContainer, "Peking Duck")
+  
   CreateCuisine("Indian")
+  const indianContainer = document.getElementById("indian-container")
+  CreateFoodItem(indianContainer, "Pani Puri")
+  CreateFoodItem(indianContainer, "Tandoori")
+  CreateFoodItem(indianContainer, "Tikka Masala")
 })
 
 
