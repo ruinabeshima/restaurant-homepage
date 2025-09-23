c57509cd620b691b008eb9f328e40411aeba56fd
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-09-23 10:06:05 +0900

    Added HTML script and CSS for the header, and the title plus description

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  // The output mini file in "dist"
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
@@ -25,6 +26,14 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      }
     ],
   },
 };
